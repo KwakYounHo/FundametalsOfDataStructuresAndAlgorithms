8e8741d2c185f78c99a1dcbec04248a7813379a4
곽윤호 <kwakdm95@gmail.com>
2025-02-20 14:41:14 +0900

    Optimize calculations

--- Algorithms/Euclidean.py
+++ Algorithms/Euclidean.py
@@ -10,21 +10,28 @@
 # 		return max(x,y)
 	
 # Professors implementation - gcd_sub
-# def gcd(x, y):
-# 	while x != 0 and y != 0:
-# 		if x > y:
-# 			x = x-y
-# 		else:
-# 			y = y-x
-# 	return x + y
-
-# Professors implementation - gcd_mod
-def gcd(x, y):
+def gcd_subtract(x, y):
 	while x != 0 and y != 0:
 		if x > y:
-			x = x % y
+			x = x-y
 		else:
-			y = y % x
-	return x + y
-	
-print(gcd(32, 77))
+			y = y-x
+	return max(x, y)
+
+# Professors implementation - gcd_mod
+def gcd_modular(x, y):
+	while y != 0:
+		x, y = y, x % y
+	return x
+
+# Professors implementation - gcd_rec
+def gcd_recursive(x, y):
+	if y == 0:
+		return x
+	else:
+		return gcd_recursive(y, x % y)
+	# return x if y == 0 else gcd_recursive(y, x % y)
+
+print(gcd_subtract(32, 77))
+print(gcd_modular(2, 100))
+print(gcd_recursive(12, 48))
